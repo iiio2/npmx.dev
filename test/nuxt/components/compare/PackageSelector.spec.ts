8be460e26ea0096a4dba0e2a5822575ddb8ce6ec
Matteo Gabriele <m.gabriele.dev@gmail.com>
2026-02-08 13:39:53 +0100

    fix(ui): prevent compare with non-existing package (#1207)

--- test/nuxt/components/compare/PackageSelector.spec.ts
+++ test/nuxt/components/compare/PackageSelector.spec.ts
@@ -153,13 +153,29 @@ describe('PackageSelector', () => {
       })
 
       const input = component.find('input')
-      await input.setValue('my-package')
+      await input.setValue('lodash')
       await input.trigger('keydown', { key: 'Enter' })
       await flushPromises()
 
       const emitted = component.emitted('update:modelValue')
       expect(emitted).toBeTruthy()
-      expect(emitted![0]![0]).toEqual(['my-package'])
+      expect(emitted![0]![0]).toEqual(['lodash'])
+    })
+
+    it('adds "no dep" entry on Enter key', async () => {
+      const component = await mountSuspended(PackageSelector, {
+        props: {
+          modelValue: [],
+        },
+      })
+
+      const input = component.find('input')
+      await input.setValue('no dep')
+      await input.trigger('keydown', { key: 'Enter' })
+
+      const emitted = component.emitted('update:modelValue')
+      expect(emitted).toBeTruthy()
+      expect(emitted![0]![0]).toEqual(['__no_dependency__'])
     })
 
     it('clears input after adding package', async () => {
@@ -172,7 +188,7 @@ describe('PackageSelector', () => {
       })
 
       const input = component.find('input')
-      await input.setValue('my-package')
+      await input.setValue('lodash')
       await input.trigger('keydown', { key: 'Enter' })
       await flushPromises()
 
